a3cb229ce52b868813f3c6e8fd95cedbc8c6cc8d
HunterECampbell <hcnureth@gmail.com>
2025-02-07 16:47:17 -0700

    Add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
         ],
       },
     }),
+    svgr({ svgrOptions: { memo: true }, include: "**/*.svg?react" }),
   ],
   server: {
     port: 9000,
