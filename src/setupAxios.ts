dca29003564af460c49ed9ea68e1263db845d1a3
HunterECampbell <hcnureth@gmail.com>
2025-02-07 16:44:11 -0700

    Cleanup

--- src/setupAxios.ts
+++ src/setupAxios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 export function setupAxios(): AxiosInstance {
   const axiosInstance = axios.create({
-    baseURL: "",
+    baseURL: "https://frontend-take-home-service.fetch.com",
   });
 
   axiosInstance.defaults.withCredentials = true;
