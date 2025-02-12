e411eec184ff39461d177deaa5ee2e3a49e5787a
HunterECampbell <hcnureth@gmail.com>
2025-02-11 23:05:45 -0700

    Add confetti

--- src/components/MatchedDogCard.tsx
+++ src/components/MatchedDogCard.tsx
@@ -3,6 +3,7 @@ import { styled as muiStyled } from "@mui/material/styles";
 import { mediaQueryBreakpoint } from "../consts/DeviceBreakpoints";
 import { useDogsStore } from "../stores/dogs";
 import { useTranslation } from "react-i18next";
+import confetti from "canvas-confetti";
 
 import Box from "@mui/material/Box";
 import DogCard from "../components/DogCard";
@@ -10,6 +11,35 @@ import DogCard from "../components/DogCard";
 const MatchedDogCard = () => {
   const { t } = useTranslation();
   const { matchedDog } = useDogsStore();
+
+  const durationInMilliseconds = 1500;
+  const animationEnd = Date.now() + durationInMilliseconds;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  const randomInRange = (min: number, max: number) => {
+    return Math.random() * (max - min) + min;
+  };
+
+  const interval = setInterval(() => {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 40 * (timeLeft / durationInMilliseconds);
+    // since particles fall down, start a bit higher than random
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    });
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    });
+  }, 250);
 
   return (
     <MatchDogWrapper>
